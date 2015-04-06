580977c905a30b390d2239f206c36f1bf3083052
dylanlott <lott.dylan@gmail.com>
2015-04-06 10:07:38 -0600

    javascript practice problem updates

---
+++ morning-practice-problems/finder.js
@@ -0,0 +1,11 @@
+var nums = ["hello", "javascript", "awesome"]
+
+var finder = function(str, arr){
+	for(i=0; i<arr.length; i++){
+		if(str === arr[i]){
+			return true; 
+		}else{
+			return false; 
+		}
+	}
+}
